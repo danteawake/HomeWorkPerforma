028a9adb6336e8a373cb95536efdcce245f28948
Oleg Egorov <danteawake88@gmail.com>
2026-03-13 09:04:07 +0400

    Homework1_1

---
+++ src/homework1/Homework1_1.java
@@ -0,0 +1,22 @@
+package homework1;
+
+public class Homework1_1 {
+
+     /*
+      Задача 1:
+      Написать приложение, которое будет вычислять и выводить значение по формуле:
+      a=4*(b+c-1)/2;
+      b и c задаем в коде самостоятельно.
+     */
+
+    public static void main(String[] args) {
+
+        int b = 6;
+        int c = 3;
+
+        int a = 4 * (b + c - 1) / 2;
+
+        System.out.println(a);
+
+    }
+}
