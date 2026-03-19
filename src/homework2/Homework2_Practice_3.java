f48ecba57f1a2306d6aaeb7fb7dbf6131b5bd909
Oleg Egorov <danteawake88@gmail.com>
2026-03-19 18:40:43 +0400

    Practice 2_3

--- src/homework2/Homework2_Practice_3.java
+++ src/homework2/Homework2_Practice_3.java
@@ -9,8 +9,6 @@ public class Homework2_Practice_3 {
      */
 
     public static void main(String[] args) {
-        int n = 0;
-
         for (int i = 0; i <= 20; i += 2) {
             if (i > 10) System.out.println(i);
         }
