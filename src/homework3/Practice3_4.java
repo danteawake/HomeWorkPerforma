6beb645d84ab129ffb813a24cb9306203fb80c0b
Oleg Egorov <danteawake88@gmail.com>
2026-03-20 10:37:05 +0400

    Practice3_4

---
+++ src/homework3/Practice3_4.java
@@ -0,0 +1,39 @@
+package homework3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Practice3_4 {
+
+    /*
+    Задача 1:
+    Посчитать сумму четных элементов стоящих на главной диагонали.
+     */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        Random random = new Random();
+        int sum = 0;
+
+        int[][] twoDimArray = new int[number][number];
+        for (int i = 0; i < twoDimArray.length; i++) {
+            for (int j = 0; j < twoDimArray[i].length; j++) {
+                twoDimArray[i][j] = random.nextInt(50);
+                if (twoDimArray[i][j] < 10) {
+                    System.out.print(twoDimArray[i][j] + "  ");
+                } else {
+                    System.out.print(twoDimArray[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < twoDimArray.length; i++) {
+            if (twoDimArray[i][i] % 2 == 0) {
+                sum += twoDimArray[i][i];
+            }
+        }
+        System.out.println("Main diagonal even numbers sum = " + sum);
+    }
+}
