4417705fd48baca699ded13364ac1d08a0c5d05c
Oleg Egorov <danteawake88@gmail.com>
2026-03-20 10:43:58 +0400

    Practice3_5

---
+++ src/homework3/Practice3_5.java
@@ -0,0 +1,43 @@
+package homework3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Practice3_5 {
+
+    /*
+    Задача 2:
+    Вывести нечетные элементы находящиеся под главной диагональю
+    (включительно)
+     */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        Random random = new Random();
+        int sum = 0;
+
+        int[][] twoDimArray = new int[number][number];
+        for (int i = 0; i < twoDimArray.length; i++) {
+            for (int j = 0; j < twoDimArray[i].length; j++) {
+                twoDimArray[i][j] = random.nextInt(5);
+                if (twoDimArray[i][j] < 10) {
+                    System.out.print(twoDimArray[i][j] + "  ");
+                } else {
+                    System.out.print(twoDimArray[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < twoDimArray.length; i++) {
+            for (int j = 0; j < twoDimArray[i].length; j++) {
+                if (j <= i) {
+                    sum += twoDimArray[i][j];
+                }
+            }
+        }
+
+        System.out.println("Sum of number under main diagonal inclusive = " + sum);
+    }
+}
